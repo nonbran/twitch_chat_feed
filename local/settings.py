2885388a47d17d69a17d485758088bdff0786d49
nonbran <74632635+nonBran@users.noreply.github.com>
2021-05-26 23:10:29 -0400

    wip: configparser read config

--- local/settings.py
+++ local/settings.py
@@ -1,9 +1,21 @@
-from constant.filename import fn_secrets
+from configparser import RawConfigParser
+from constant.filename import dn_settings, fn_secrets, fn_settings
 from os import getcwd
+from os import stat
+from os.path import exists
 from pathlib import Path
 # init paths
-local_path = Path(getcwd())
-project_path = local_path.parent
+runtime_path = Path(getcwd())
+project_path = runtime_path.parent
 user_path = project_path.parent.parent
 # init filenames
 path_secrets = Path(user_path, fn_secrets)
+path_settings = Path(project_path, dn_settings, fn_settings)
+# TODO WIP init configparser & read settings.cfg
+# path_stat = stat(path_settings)
+# path_exists = True if exists(path_settings) else False
+# ocp = RawConfigParser() if path_exists else None
+# if ocp is None:
+#     print(f'\nno settings.cfg found at path')
+#     exit()
+pass
