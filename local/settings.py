84cccb0e4892810f07e1ec973d8ad3c9bd981e35
nonbran <74632635+nonBran@users.noreply.github.com>
2021-05-26 23:21:43 -0400

    relocate secrets

--- local/settings.py
+++ local/settings.py
@@ -7,10 +7,11 @@ from pathlib import Path
 # init paths
 runtime_path = Path(getcwd())
 project_path = runtime_path.parent
+python_path = project_path.parent
 user_path = project_path.parent.parent
 # init filenames
-path_secrets = Path(user_path, fn_secrets)
-path_settings = Path(project_path, dn_settings, fn_settings)
+path_secrets = Path(python_path, fn_secrets)
+# path_settings = Path(project_path, dn_settings, fn_settings)
 # TODO WIP init configparser & read settings.cfg
 # path_stat = stat(path_settings)
 # path_exists = True if exists(path_settings) else False
