9c8bc49db12b3b0588da4d39ce50279d9baa2f11
nonbran <74632635+nonBran@users.noreply.github.com>
2021-06-03 20:22:18 -0400

    del: deprecated configparser wip code

--- local/settings.py
+++ local/settings.py
@@ -12,12 +12,4 @@ python_path = project_path.parent
 user_path = project_path.parent.parent
 # init filenames
 path_secrets = Path(python_path, fn_secrets)
-# path_settings = Path(project_path, dn_settings, fn_settings)
-# TODO WIP init configparser & read settings.cfg
-# path_stat = stat(path_settings)
-# path_exists = True if exists(path_settings) else False
-# ocp = RawConfigParser() if path_exists else None
-# if ocp is None:
-#     print(f'\nno settings.cfg found at path')
-#     exit()
 pass
